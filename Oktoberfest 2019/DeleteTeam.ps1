eef3ebb2eb3690cd0af5c34ff2223ac218399049
Lee Ford <leeford@users.noreply.github.com>
2019-10-14 20:33:15 +0100

    Oktoberfest

---
+++ Oktoberfest 2019/DeleteTeam.ps1
@@ -0,0 +1,55 @@
+function Invoke-GraphAPICall {
+
+    param (
+
+        [Parameter(mandatory = $true)][uri]$URI,
+        [Parameter(mandatory = $false)][string]$method,
+        [Parameter(mandatory = $false)][string]$body
+
+    )
+
+    # Is method speficied (if not assume GET)
+    if ([string]::IsNullOrEmpty($method)) { $method = 'GET' }
+
+    $Headers = @{"Authorization" = "Bearer $($global:token.access_token)" }
+
+    # Paging
+    $currentUri = $URI
+    $content = while (-not [string]::IsNullOrEmpty($currentUri)) {
+
+        # API Call
+        $apiCall = try {
+            
+            Invoke-RestMethod -Method $method -Uri $currentUri -ContentType "application/json" -Headers $Headers -Body $body -ResponseHeadersVariable script:responseHeaders
+
+        }
+        catch {
+            
+            $errorMessage = $_.ErrorDetails.Message | ConvertFrom-Json
+
+        }
+        
+        $currentUri = $null
+    
+        if ($apiCall) {
+    
+            # Check if any data is left
+            $currentUri = $apiCall.'@odata.nextLink'
+    
+            $apiCall
+    
+        }
+    
+    }
+
+    return $content
+    
+}
+
+# Delete Team(s)
+#######################################################################################################
+
+# Delete
+Invoke-GraphAPICall -URI "https://graph.microsoft.com/v1.0/groups/$($global:TeamId)" -Method "DELETE"
+
+#######################################################################################################
---
+++ Oktoberfest 2019/ObtainToken.ps1
@@ -0,0 +1,52 @@
+$global:token = $null
+
+# Tenant ID, Client ID
+$tenantId = "346e50a9-e410-4af6-b468-bf406fad043e"
+$clientId = "85b3dc2c-a2c3-47bd-9293-d2b665f7029d"
+
+$codeBody = @{ 
+
+    resource  = "https://graph.microsoft.com/"
+    client_id = $clientId
+    scope     = "Group.ReadWrite.All, User.ReadBasic.All"
+
+}
+
+# Get OAuth Code
+$codeRequest = Invoke-RestMethod -Method POST -Uri "https://login.microsoftonline.com/$tenantId/oauth2/devicecode" -Body $codeBody
+
+# Print Code to host
+Write-Host "`n$($codeRequest.message)"
+
+$tokenBody = @{
+
+    grant_type = "urn:ietf:params:oauth:grant-type:device_code"
+    code       = $codeRequest.device_code
+    client_id  = $clientId
+
+}
+
+# Get OAuth Token
+while ([string]::IsNullOrEmpty($tokenRequest.access_token)) {
+
+    $tokenRequest = try {
+
+        Invoke-RestMethod -Method POST -Uri "https://login.microsoftonline.com/$tenantId/oauth2/token" -Body $tokenBody
+
+    }
+    catch {
+
+        $errorMessage = $_.ErrorDetails.Message | ConvertFrom-Json
+
+        # If not waiting for auth, throw error
+        if ($errorMessage.error -ne "authorization_pending") {
+
+            Throw
+
+        }
+
+    }
+
+}
+
+$global:token = $tokenRequest
